b7e77d0deea929edc27d6567187a39b4b5fa650e
khangich <khangich@gmail.com>
2014-10-15 12:38:35 +0700

    add faw page, navigation

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,4 +8,17 @@ $(document).ready(function(){
       }
     });
 
+    console.log(window.location.hash)
+
+    if(window.location.hash != ''){
+    	if($('iframe[name=content]').size() > 0){
+    		$('iframe[name=content]').attr('src', window.location.hash.substring(1))
+    	}
+    }
+
+    $('.iframe-links a').click(function(){
+    	var link = $(this).attr('href')
+    	window.location.hash = link;
+    })
+
 });
