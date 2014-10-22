41d7d3ff8308db9ae6339e54ecb821ff57c525c2
khangich <khangich@gmail.com>
2014-10-22 15:43:01 +0700

    clean up scripts

--- examples/exe/stop-local-spark-cluster.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-while true ; do
-	PID=`jps | egrep "Master|Worker" | head -1 | cut -d' ' -f1`
-	[ "X$PID" == "X" ] && exit 0
-	echo -n "Killing "
-	ps www $PID | tail -1
-	kill $PID
-	sleep 1
-done
