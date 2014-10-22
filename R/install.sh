870d8bc37ff277c2e56f7dcc704cacca102f7b61
Nhan Vu <nhanitvn@gmail.com>
2014-10-22 12:37:54 +0700

    check and copy assembly file only

--- R/install.sh
+++ R/install.sh
@@ -8,7 +8,7 @@ SCALA_VERSION="2.10"
 SPARK_VERSION="0.9"
 TARGET_DIR="$FWDIR/../spark/target/scala-$SCALA_VERSION"
 
-if [ ! -f "$TARGET_DIR/ddf_spark_$SCALA_VERSION-$SPARK_VERSION.jar" ]; then
+if [ ! -f "$TARGET_DIR/ddf_spark-assembly-$SPARK_VERSION.jar" ]; then
   echo "You must build DDF's Java core and spark package firstly" 
   echo "Guide: Go to $(cd $FWDIR/../; pwd) and run this command"
   echo "mvn package install [-DskipTests]"
@@ -16,8 +16,7 @@ if [ ! -f "$TARGET_DIR/ddf_spark_$SCALA_VERSION-$SPARK_VERSION.jar" ]; then
 fi
 
 rm -r package/inst/java/* || mkdir package/inst/java
-cp $TARGET_DIR/ddf_spark_$SCALA_VERSION-$SPARK_VERSION.jar package/inst/java/
-cp $TARGET_DIR/lib/*.jar package/inst/java/
+cp $TARGET_DIR/ddf_spark-assembly-$SPARK_VERSION.jar package/inst/java/
 
 # Install R
 R CMD INSTALL package/
