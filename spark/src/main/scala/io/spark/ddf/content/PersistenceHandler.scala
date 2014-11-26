91f92ba66a5976afeabe5caa7e7f57321ce760d6
huandao0812 <huan@adatau.com>
2014-11-26 12:21:34 -0800

    change persistenceUri

--- spark/src/main/scala/io/spark/ddf/content/PersistenceHandler.scala
+++ spark/src/main/scala/io/spark/ddf/content/PersistenceHandler.scala
@@ -19,11 +19,11 @@ class PersistenceHandler(ddf: DDF) extends BPersistenceHandler(ddf) {
   override def persist(doOverwrite: Boolean): PersistenceUri = {
     val dataFile = this.getDataFileName()
     val schemaFile = this.getSchemaFileName()
-
+    val folderPath = this.getFolderPath(ddf.getNamespace, ddf.getName, null)
     Utils.writeToFile(schemaFile, JsonSerDes.serialize(this.getDDF.getSchema) + "\n")
     val schemaRDD = ddf.getRepresentationHandler.get(classOf[SchemaRDD]).asInstanceOf[SchemaRDD]
     schemaRDD.saveAsParquetFile(dataFile)
-    new PersistenceUri(ddf.getEngine, dataFile)
+    new PersistenceUri(ddf.getEngine, folderPath)
   }
 
   override def getDataFileName(): String = {
@@ -36,7 +36,11 @@ class PersistenceHandler(ddf: DDF) extends BPersistenceHandler(ddf) {
 
   def getFolderPath(namespace: String, name: String, subfolder: String) = {
     val directory = this.locateOrCreatePersistenceSubdirectory(namespace)
-    s"$directory/$name/$subfolder"
+    if(subfolder != null || !subfolder.isEmpty) {
+      s"$directory/$name/$subfolder"
+    } else {
+      s"$directory/$name"
+    }
   }
   override def load(namespace: String, name: String): IPersistible = {
     val schemaPath = this.getFolderPath(namespace, name, "schema")
