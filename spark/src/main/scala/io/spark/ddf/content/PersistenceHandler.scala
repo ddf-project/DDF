a5b1d4ab7eb92b397f956b375896db79b00158e9
huandao0812 <huan@adatau.com>
2014-11-26 11:38:11 -0800

    add method to create ddf from parquet file

--- spark/src/main/scala/io/spark/ddf/content/PersistenceHandler.scala
+++ spark/src/main/scala/io/spark/ddf/content/PersistenceHandler.scala
@@ -2,10 +2,13 @@ package io.spark.ddf.content
 
 import io.basic.ddf.content.{PersistenceHandler => BPersistenceHandler}
 import io.ddf.DDF
+import io.ddf.content.Schema
 import io.ddf.content.APersistenceHandler.PersistenceUri
 import io.ddf.util.Utils
 import io.ddf.util.Utils.JsonSerDes
 import org.apache.spark.sql.SchemaRDD
+import io.spark.ddf.SparkDDFManager
+import io.ddf.content.IHandlePersistence.IPersistible
 
 /**
  * author: daoduchuan
@@ -24,16 +27,26 @@ class PersistenceHandler(ddf: DDF) extends BPersistenceHandler(ddf) {
   }
 
   override def getDataFileName(): String = {
-    val nameSpace = ddf.getNamespace
-    val directory = this.locateOrCreatePersistenceSubdirectory(nameSpace)
-    val ddfName = this.getDDF.getName
-    s"$directory/$ddfName/data"
+    this.getFolderPath(ddf.getNamespace, ddf.getName, "data")
   }
 
   override def getSchemaFileName(): String = {
-    val nameSpace = ddf.getNamespace
-    val directory = this.locateOrCreatePersistenceSubdirectory(nameSpace)
-    val ddfName = this.getDDF.getName
-    s"$directory/$ddfName/schema"
+    this.getFolderPath(ddf.getNamespace, ddf.getName, "schema")
+  }
+
+  def getFolderPath(namespace: String, name: String, subfolder: String) = {
+    val directory = this.locateOrCreatePersistenceSubdirectory(namespace)
+    s"$directory/$name/$subfolder"
+  }
+  override def load(namespace: String, name: String): IPersistible = {
+    val schemaPath = this.getFolderPath(namespace, name, "schema")
+    val dataPath = this.getFolderPath(namespace, name, "data")
+    val manager = this.getManager
+    val ctx = manager.asInstanceOf[SparkDDFManager].getHiveContext
+    val schemaRDD = ctx.parquetFile(dataPath)
+    val schema = JsonSerDes.loadFromFile(schemaPath).asInstanceOf[Schema]
+
+    val ddf = manager.newDDF(manager, schemaRDD, Array(classOf[SchemaRDD]), manager.getNamespace, null, schema)
+    ddf
   }
 }
