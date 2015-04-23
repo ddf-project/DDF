e0ad2cc161cdee618ac9f3e0e57e30c262c7fd70
Dao Duc Huan <huan@adatao.com>
2015-04-23 15:05:04 +0700

    use DDFUtils.generateObjectName to gen ddf's uuid

--- core/src/main/java/io/ddf/DDF.java
+++ core/src/main/java/io/ddf/DDF.java
@@ -43,6 +43,7 @@ import io.ddf.types.AGloballyAddressable;
 import io.ddf.types.AggregateTypes.AggregateField;
 import io.ddf.types.AggregateTypes.AggregationResult;
 import io.ddf.types.IGloballyAddressable;
+import io.ddf.util.DDFUtils;
 import io.ddf.util.ISupportPhantomReference;
 import io.ddf.util.PhantomReference;
 
@@ -171,7 +172,7 @@ public abstract class DDF extends ALoggable //
 
     this.setName(name);
 
-    this.uuid = UUID.randomUUID().toString().replace("-", "_");
+    this.uuid = DDFUtils.generateObjectName(this);
     // Facades
     this.ML = new MLFacade(this, this.getMLSupporter());
     this.VIEWS = new ViewsFacade(this, this.getViewHandler());
