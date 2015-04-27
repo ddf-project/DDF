012a2057f6172312e45763edaa2254a9271a5e4e
Dao Duc Huan <huan@adatao.com>
2015-04-27 18:06:33 +0700

    fix ListDDFSuite

--- spark/src/test/scala/io/spark/ddf/content/ListDDFSuite.scala
+++ spark/src/test/scala/io/spark/ddf/content/ListDDFSuite.scala
@@ -11,7 +11,8 @@ class ListDDFSuite extends ATestSuite {
   test("test list ddf") {
     val ddf1 = manager.sql2ddf("select * from mtcars")
     val ddf2 = manager.sql2ddf("select * from airline")
-
+    ddf1.setName("mtcars")
+    ddf2.setName("airline")
     manager.addDDF(ddf2)
 
     val listDDF = manager.listDDFs()
