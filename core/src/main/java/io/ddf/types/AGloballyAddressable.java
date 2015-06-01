3b9706897ce77d75e720ec25a7ea9cf77ed66b6b
Dao Duc Huan <huan@adatao.com>
2015-06-01 09:16:00 +0700

    fix ddf getUri
    when ddf’s name is null, getUri return null

--- core/src/main/java/io/ddf/types/AGloballyAddressable.java
+++ core/src/main/java/io/ddf/types/AGloballyAddressable.java
@@ -4,7 +4,11 @@ package io.ddf.types;
 public abstract class AGloballyAddressable implements IGloballyAddressable {
 
   public static String getUri(IGloballyAddressable obj) {
-    return String.format("%s://%s/%s", obj.getGlobalObjectType(), obj.getNamespace(), obj.getName());
+    if(obj.getName() == null || obj.getName().isEmpty()) {
+      return null;
+    } else {
+      return String.format("%s://%s/%s", obj.getGlobalObjectType(), obj.getNamespace(), obj.getName());
+    }
   }
 
   @Override
