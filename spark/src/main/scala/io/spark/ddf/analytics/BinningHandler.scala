a8090c4c1dc63e930d6255f47da8cadaa3f4c0a8
namma <namma@usc.edu>
2015-06-01 16:13:14 +0700

    update dependencies of sql2txt in BinningHandler.scala

--- spark/src/main/scala/io/spark/ddf/analytics/BinningHandler.scala
+++ spark/src/main/scala/io/spark/ddf/analytics/BinningHandler.scala
@@ -148,8 +148,7 @@ class BinningHandler(mDDF: DDF) extends ABinningHandler(mDDF) with IHandleBinnin
 
   def getIntervalsFromNumBins(colName: String, bins: Int): Array[Double] = {
     val cmd = "SELECT min(%s), max(%s) FROM %s".format(colName, colName, mDDF.getTableName)
-    val res: Array[Double] = mDDF.sql2txt(cmd, "").get(0).replace("ArrayBuffer", "").
-      replace("(", "").replace(")", "").replace(", ", "\t").split("\t").map(x ⇒ x.toDouble)
+    val res: Array[Double] = mDDF.sql2txt(cmd, "").get(0).split("\t").map(x ⇒ x.toDouble)
     val (min, max) = (res(0), res(1))
     val eachInterval = (max - min) / bins
     val probs: Array[Double] = Array.fill[Double](bins + 1)(0)
@@ -182,7 +181,7 @@ class BinningHandler(mDDF: DDF) extends ABinningHandler(mDDF) with IHandleBinnin
     pArray.foreach(x ⇒ cmd = cmd + x.toString + ",")
     cmd = cmd.take(cmd.length - 1)
     cmd = String.format("min(%s), percentile_approx(%s, array(%s)), max(%s)", colName, colName, cmd, colName)
-    mDDF.sql2txt("SELECT " + cmd + " FROM @this", "").get(0).replaceAll("ArrayBuffer|\\(|\\)|,", "").split("\t| ").
+    mDDF.sql2txt("SELECT " + cmd + " FROM @this", "").get(0).replaceAll("\\[|\\]| ", "").replaceAll(",", "\t").split("\t").
       map(x ⇒ x.toDouble)
   }
 
