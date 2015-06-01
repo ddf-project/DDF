81e8b78c404fc27bcaa9cee81b90dd334bed36f3
Dao Duc Huan <huan@adatao.com>
2015-06-01 13:47:34 +0700

    DDF.getUri return null if name is null

--- core/src/main/java/io/ddf/types/AGloballyAddressable.java
+++ core/src/main/java/io/ddf/types/AGloballyAddressable.java
@@ -1,10 +1,11 @@
 package io.ddf.types;
 
+import com.google.common.base.Strings;
 
 public abstract class AGloballyAddressable implements IGloballyAddressable {
 
   public static String getUri(IGloballyAddressable obj) {
-    if(obj.getName() == null || obj.getName().isEmpty()) {
+    if(Strings.isNullOrEmpty(obj.getName())) {
       return null;
     } else {
       return String.format("%s://%s/%s", obj.getGlobalObjectType(), obj.getNamespace(), obj.getName());
