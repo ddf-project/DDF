04a3812c4b55ecfc14426bfa6ac1ae799778aee0
namma <namma@usc.edu>
2015-06-03 18:34:25 +0700

    update unit test SampleSuite to add test with percentage

--- spark/src/test/scala/io/spark/ddf/content/SampleSuite.scala
+++ spark/src/test/scala/io/spark/ddf/content/SampleSuite.scala
@@ -6,7 +6,7 @@ import scala.collection.JavaConversions._
  */
 class SampleSuite extends ATestSuite {
   createTableMtcars()
-  test("test sample") {
+  test("test sample with numrows") {
     val ddf = manager.sql2ddf("select * from mtcars")
     val sample = ddf.VIEWS.getRandomSample(10)
 
@@ -14,5 +14,12 @@ class SampleSuite extends ATestSuite {
     assert(sample(1)(0).asInstanceOf[Double] != sample(2)(0).asInstanceOf[Double])
     assert(sample(2)(0).asInstanceOf[Double] != sample(3)(0).asInstanceOf[Double])
     assert(sample.length == 10)
+  }
+
+  test("test sample with percentage") {
+    val ddf = manager.sql2ddf("select * from mtcars")
+    val sample = ddf.VIEWS.getRandomSample(0.5, false, 1)
+    //sample.getSchema.getColumns.foreach(c => {println(c.getName + " - " + c.getType)})
+    sample.VIEWS.head(3).foreach(println)
   }
 }
