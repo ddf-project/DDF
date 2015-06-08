77e350cb60b648668518fff96c5974a523b7f65d
Dao Duc Huan <huan@adatao.com>
2015-06-08 13:34:31 +0700

    Revert "Merge pull request #123 from ddf-project/revert-121-ddfSpark"
    This reverts commit 4371e274fb08d5bd005be5b2bdc0093286d37569, reversing
    changes made to d1bf1ad34558a8f3b50f17d66a0f9297911f2db2.

---
+++ spark/src/main/scala/io/ddf/spark/content/RDDInt2RDDRow.scala
@@ -0,0 +1,21 @@
+package io.ddf.spark.content
+
+import io.ddf.DDF
+import io.ddf.content.{Representation, ConvertFunction}
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.Row
+
+/**
+ * Created by huandao on 5/26/15.
+ */
+class RDDInt2RDDRow(@transient ddf: DDF) extends ConvertFunction(ddf) {
+
+  override def apply(representation: Representation): Representation = {
+    val rddRow = representation.getValue match {
+      case rdd: RDD[Int] => {
+        rdd.map{i => Row(i)}
+      }
+    }
+    new Representation(rddRow, RepresentationHandler.RDD_ROW.getTypeSpecsString)
+  }
+}
