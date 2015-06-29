598f8c718c946aeb30b63fdf5c0ec3a33e8491f4
Dao Duc Huan <huan@adatao.com>
2015-06-29 18:47:44 +0700

    add TextFileFormat

---
+++ core/src/main/java/io/ddf/datasource/FileFormat.java
@@ -0,0 +1,18 @@
+package io.ddf.datasource;
+
+
+/**
+ */
+
+public class FileFormat {
+
+  private DataFormat format;
+
+  public DataFormat getFormat() {
+    return this.format;
+  }
+
+  public void setFormat(DataFormat format) {
+    this.format = format;
+  }
+}
