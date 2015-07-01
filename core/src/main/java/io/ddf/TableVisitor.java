db3027d88e576623b73c303b69a173df7aaaafce
jing <PangZhi@github.com>
2015-06-30 17:36:16 -0700

    add support for strong typed result and parser

---
+++ core/src/main/java/io/ddf/TableVisitor.java
@@ -0,0 +1,10 @@
+package io.ddf;
+
+/**
+ * Created by jing on 6/29/15.
+ * This class is used to visit all the table names that appear in the SQL
+ * statement. By overriding the visit function, we can do different operations
+ * on the table.
+ */
+public class TableVisitor {
+}
---
+++ core/src/main/java/io/ddf/content/SqlTypedCell.java
@@ -0,0 +1,7 @@
+package io.ddf.content;
+
+/**
+ * Created by jing on 6/30/15.
+ */
+public class SqlTypedCell {
+}
