528140b4f91c4382ad2aae258b18807756ccfc3c
Vu Pham <phvu225@gmail.com>
2015-07-03 12:02:04 -0700

    restructure pyddf codebase

--- python/examples/basics.py
+++ python/examples/basics.py
@@ -1,28 +1,27 @@
-from ddf.DDFManager import DDFManager
-dm = DDFManager("spark")
+from ddf import DDFManager, DDF_HOME
 
-import os
-DDF_HOME = os.getenv("DDF_HOME")
+dm = DDFManager('spark')
 
-dm.sql("set hive.metastore.warehouse.dir=/tmp/hive/warehouse")
-dm.sql("drop table if exists mtcars")
-dm.sql("CREATE TABLE mtcars (mpg double, cyl int, disp double, hp int, drat double, wt double, qesc double, vs int, am int, gear int, carb string) ROW FORMAT DELIMITED FIELDS TERMINATED BY ' '")
-dm.sql("LOAD DATA LOCAL INPATH '" + DDF_HOME  + "/resources/test/mtcars' INTO TABLE mtcars")
+dm.sql('set hive.metastore.warehouse.dir=/tmp/hive/warehouse')
+dm.sql('drop table if exists mtcars')
+dm.sql("CREATE TABLE mtcars (mpg double, cyl int, disp double, hp int, drat double, wt double,"
+       " qesc double, vs int, am int, gear int, carb string) ROW FORMAT DELIMITED FIELDS TERMINATED BY ' '")
+dm.sql("LOAD DATA LOCAL INPATH '" + DDF_HOME + "/resources/test/mtcars' INTO TABLE mtcars")
 
-ddf = dm.sql2ddf("select * from mtcars")
+ddf = dm.sql2ddf('select * from mtcars')
 
-ddf.getColumnNames()
+print('Columns: ' + ', '.join(ddf.colnames))
 
-ddf.getNumColumns()
-ddf.getNumRows()
+print('Number of columns: {}'.format(ddf.cols))
+print('Number of rows: {}'.format(ddf.rows))
 
-ddf.getSummary()
+ddf.summary()
 
-ddf.firstNRows(10)
+ddf.head(10)
 
-ddf.aggregate("sum(mpg), min(hp)", "vs, am")
+ddf.aggregate('sum(mpg), min(hp)', 'vs, am')
 
-ddf.getFiveNumSummary()
+ddf.five_nums()
 
 ddf.sample(10)
 
