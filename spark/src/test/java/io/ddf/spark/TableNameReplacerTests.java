34f0ccf6f19c42d899a768e80f3e443b3ee55a3d
jing <PangZhi@github.com>
2015-07-17 14:36:27 -0700

    add tests for table name replacer

---
+++ spark/src/test/java/io/ddf/spark/TableNameReplacerTests.java
@@ -0,0 +1,277 @@
+package io.ddf.spark;
+
+import io.ddf.DDF;
+import io.ddf.DDFManager;
+import io.ddf.TableNameReplacer;
+import io.ddf.content.Schema;
+import io.ddf.datasource.SQLDataSourceDescriptor;
+import io.ddf.exception.DDFException;
+import net.sf.jsqlparser.JSQLParserException;
+import net.sf.jsqlparser.parser.CCJSqlParserManager;
+import net.sf.jsqlparser.statement.Statement;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.StringReader;
+import java.util.Arrays;
+
+
+/**
+ * Created by jing on 6/30/15.
+ */
+public class TableNameReplacerTests {
+
+
+    public static DDFManager manager;
+    public static CCJSqlParserManager parser;
+
+
+    /**
+     * @brief Test complex query with full uri.
+     * @throws DDFException
+     */
+    @Test
+    public void testComplexQuery() throws  DDFException {
+        TableNameReplacer tableNameReplacer = new TableNameReplacer(manager);
+        String sqlcmd =
+                "With p as " +
+                "(Select * from ddf://adatao/a) " +
+                "select sum(ddf://adatao/a.depdelay) " +
+                "from ddf://adatao/a  TABLESAMPLE(100 percent), ddf://adatao/b " +
+                "on ddf://adatao.a.id = ddf:/adatao/b.id " +
+                "where ddf://adatao/a.id > 1 AND ddf://adatao/a.id < 3 or ddf://adatao/a.year > 2000 " +
+                "group by ddf://adatao/a.year " +
+                "having sum(ddf://adatao/a.depdelay) < 100 " +
+                "order by ddf://adatao/a.year";
+        Statement statement = null;
+        try {
+            statement = parser.parse(new StringReader(sqlcmd));
+        } catch (JSQLParserException e) {
+            e.printStackTrace();
+            assert(false);
+        }
+        try {
+            statement = tableNameReplacer.run(statement);
+        } catch (Exception e) {
+            e.printStackTrace();
+            assert(false);
+        }
+        assert(statement.toString().equals("WITH p AS (SELECT * FROM tablename1) " +
+                "SELECT sum(tablename1.depdelay) " +
+                "FROM tablename1 TABLESAMPLE(100 percent), tablename2 " +
+                "WHERE tablename1.id > 1 " +
+                "AND tablename1.id < 3 " +
+                "OR tablename1.year > 2000 " +
+                "GROUP BY tablename1.year " +
+                "HAVING sum(tablename1.depdelay) < 100 " +
+                "ORDER BY tablename1.year"));
+    }
+
+    @Test
+    public void testRealQuery() {
+        TableNameReplacer tableNameReplacer = new TableNameReplacer(manager);
+        String sqlcmd = "SELECT from_unixtime(round(timestamp) - 7*3600, 'HH') hour,\n" +
+                "        count(1) count,\n" +
+                "        avg(observationnum_temp) avgTemp, \n" +
+                "        avg(observationnum_feels_like) avgFeelLike,\n" +
+                "        avg(observationnum_temp_min_24hour) avgMinTemp, \n" +
+                "        avg(observationnum_temp_max_24hour) avgMaxTemp\n" +
+                "FROM cod_sample2 \n" +
+                "WHERE latitude > 30 and latitude < 40 and longtitude > -125 and longtitude < -115\n" +
+                "GROUP BY from_unixtime(round(timestamp) - 7*3600, 'HH') \n" +
+                "HAVING hour is not null\n" +
+                "ORDER BY hour";
+        try {
+            Statement statement = parser.parse(new StringReader(sqlcmd));
+            System.out.println(statement.toString());
+        } catch (JSQLParserException e) {
+            e.printStackTrace();
+            assert false;
+        }
+
+    }
+
+    /**
+     * @brief Test full uri replacement.
+     * @throws DDFException
+     */
+    @Test
+    public  void testFullURI() throws  DDFException {
+        TableNameReplacer tableNameReplacer = new TableNameReplacer(manager);
+        String sqlcmd = "select SUM(ddf://adatao/a.b) from ddf://adatao/a group by ddf://adatao/a.a";
+        Statement statement = null;
+        try {
+            statement = parser.parse(new StringReader(sqlcmd));
+        } catch (JSQLParserException e) {
+            e.printStackTrace();
+            assert(false);
+        }
+
+        try {
+            statement = tableNameReplacer.run(statement);
+        } catch (Exception e) {
+            e.printStackTrace();
+            assert(false);
+        }
+        assert(statement.toString().equals("SELECT SUM(tablename1.b) FROM tablename1 GROUP BY tablename1.a"));
+    }
+
+    /**
+     * @brief Test sql with namespace specified.
+     * @throws DDFException
+     */
+    @Test
+    public void testNamespace() throws  DDFException {
+        TableNameReplacer tableNameReplacer  = new TableNameReplacer(manager, "adatao");
+        String sqlcmd = "select a.b from a";
+        Statement statement = null;
+        try {
+            statement = parser.parse(new StringReader(sqlcmd));
+        } catch (JSQLParserException e) {
+            e.printStackTrace();
+            assert(false);
+        }
+        try {
+            tableNameReplacer.run(statement);
+        } catch (Exception e) {
+            e.printStackTrace();
+            assert(false);
+        }
+
+        assert(statement.toString().equals("SELECT tablename1.b FROM tablename1"));
+
+    }
+
+    /**
+     * @brief Test sql with list.
+     * @throws DDFException
+     */
+    @Test
+    public void testList() throws  DDFException {
+        String[] uris={"ddf://adatao/a", "ddf://adatao/b"};
+        TableNameReplacer tableNameReplacer = new TableNameReplacer(manager, Arrays.asList(uris));
+        String sqlcmd = "select {1}.a,{2}.b from {1}";
+        Statement statement = null;
+
+        try {
+            statement = parser.parse(new StringReader(sqlcmd));
+        } catch (JSQLParserException e) {
+            e.printStackTrace();
+            assert(false);
+        }
+
+        try {
+            tableNameReplacer.run(statement);
+        } catch (Exception e) {
+            e.printStackTrace();
+            assert(false);
+        }
+        assert(statement.toString().equals("SELECT tablename1.a, tablename2.b FROM tablename1"));
+    }
+
+    /**
+     * @brief Test udfs.
+     * @throws DDFException
+     */
+    @Test
+    public void testUDF() throws  DDFException {
+        String[] singleParamUDFs = {
+                "round", "floor", "ceil", "ceiling", "rand", "exp", "ln", "log10", "log2"
+        };
+
+        String[] doubleParamUDFs = {
+                "log", "pow"
+        };
+
+
+        TableNameReplacer tableNameReplacer = new TableNameReplacer(this.manager);
+        String sqlcmd = "select %s(ddf://adatao/a.year) from ddf://adatao/a";
+        String doubleSqlCmd = "select %s(ddf://adatao/a.year, ddf://adatao/a.rev) from ddf://adatao/a";
+        for (String udfname : singleParamUDFs) {
+            String newSqlCmd = String.format(sqlcmd, udfname);
+            try {
+                Statement statement = parser.parse(new StringReader(newSqlCmd));
+                statement = tableNameReplacer.run(statement);
+                assert (statement.toString().toLowerCase().equals(
+                        String.format("select %s(tablename1.year) from tablename1", udfname)
+                ));
+            } catch (JSQLParserException e) {
+                e.printStackTrace();
+                assert(false);
+            } catch (Exception e) {
+                e.printStackTrace();
+                assert false;
+            }
+        }
+
+        for (String udfname : doubleParamUDFs) {
+            String newSqlCmd = String.format(doubleSqlCmd, udfname);
+            try {
+                Statement statement = parser.parse(new StringReader(newSqlCmd));
+                statement = tableNameReplacer.run(statement);
+                assert (statement.toString().toLowerCase().equals(
+                        String.format("select %s(tablename1.year, tablename1.rev) from tablename1",
+                                udfname)));
+            } catch (Exception e) {
+                e.printStackTrace();
+                assert false;
+            }
+        }
+
+    }
+
+    /**
+     * @brief Test ordinary spark query.
+     * @throws DDFException
+     */
+    @Test
+    public  void testLoading() throws  DDFException {
+        SQLDataSourceDescriptor sqlDataSourceDescriptor = new SQLDataSourceDescriptor(null, "SparkSQL", null, null, null);
+        manager.sql("drop table if exists airline", sqlDataSourceDescriptor);
+
+        manager.sql("create table airline (Year int,Month int,DayofMonth int,"
+                + "DayOfWeek int,DepTime int,CRSDepTime int,ArrTime int,"
+                + "CRSArrTime int,UniqueCarrier string, FlightNum int, "
+                + "TailNum string, ActualElapsedTime int, CRSElapsedTime int, "
+                + "AirTime int, ArrDelay int, DepDelay int, Origin string, "
+                + "Dest string, Distance int, TaxiIn int, TaxiOut int, Cancelled int, "
+                + "CancellationCode string, Diverted string, CarrierDelay int, "
+                + "WeatherDelay int, NASDelay int, SecurityDelay int, LateAircraftDelay int ) "
+               + "ROW FORMAT DELIMITED FIELDS TERMINATED BY ','", sqlDataSourceDescriptor);
+
+        manager.sql("load data local inpath '/Users/Jing/Github/DDF/resources/test/airline.csv' into table airline",
+                sqlDataSourceDescriptor);
+
+        DDF ddf = manager.sql2ddf("select year, month, dayofweek, deptime, arrtime,origin, distance, arrdelay, "
+                + "depdelay, carrierdelay, weatherdelay, nasdelay, securitydelay, lateaircraftdelay from airline",
+                sqlDataSourceDescriptor);
+        this.manager.setDDFName(ddf, "airlineDDF");
+        DDF sql2ddfRet = manager.sql2ddf("select * from ddf://adatao/airlineDDF");
+    }
+
+    // static Logger LOG;
+
+    @BeforeClass
+    public static void startServer() throws Exception {
+        Thread.sleep(1000);
+        // LOG = LoggerFactory.getLogger(BaseTest.class);
+        manager = DDFManager.get("spark");
+        // Add 2 test ddfs.
+        Schema schema = new Schema("tablename1", "d  d,d  d");
+        DDF ddf = manager.newDDF(manager, new Class<?>[] { DDFManager.class }, "adatao", "a",
+                schema);
+        Schema schema2 = new Schema("tablename2", "d  d,d  d");
+        DDF ddf2 = manager.newDDF(manager, new Class<?>[] { DDFManager.class }, "adatao", "b",
+                schema2);
+
+        parser = new CCJSqlParserManager();
+    }
+
+    @AfterClass
+    public static void stopServer() throws Exception {
+
+        manager.shutdown();
+    }
+
+}
