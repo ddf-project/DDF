671786fd6a54087890a8c3c1f5f9d8583610536d
freeman <freeman@adatao.com>
2015-07-22 09:00:05 +0700

    add toString to SQLResult

--- jdbc/examples/jdbc-salesforce.scala
+++ jdbc/examples/jdbc-salesforce.scala
@@ -8,17 +8,22 @@ import io.ddf.datasource.JDBCDataSourceDescriptor.JDBCDataSourceCredentials
 import io.ddf.jdbc.{JDBCDDF, JDBCDDFManager}
 import io.ddf.misc.Config.ConfigConstant
 
+
 var sourceDescriptor = new JDBCDataSourceDescriptor(
-  new DataSourceURI("jdbc:mysql://localhost:3306/pinsights"),
-  new JDBCDataSourceCredentials("root", ""),
+  new DataSourceURI("jdbc:salesforce:User=bhan@adatao.com;Password=KualaLumpur123!@#;SecurityToken=OgBwt0V2NU3fltKAse4sJdmga;"),
+  new JDBCDataSourceCredentials("bhan@adatao.com", "KualaLumpur123!@#"),
   null)
 
 val manager = new JDBCDDFManager(sourceDescriptor)
 
-val ddf = new JDBCDDF(manager, "adatao/pi", "users", "users");
+val listTables = manager.showTables()
+println(listTables)
+
+val ddf = new JDBCDDF(manager, "adatao/saleforce", "account", "Account");
 println(ddf.getColumnNames);
 
-val res = ddf.sql("select * from ddf://adatao/pi/users limit 10", "")
+val res = ddf.sql("select * from ddf://adatao/saleforce/account limit 10", "")
 println(res)
+
 
 
