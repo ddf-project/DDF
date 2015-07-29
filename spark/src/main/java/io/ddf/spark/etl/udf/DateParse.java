a1268b661086825e5055260b1f2a6956394defce
Nhan Vu <nhanitvn@gmail.com>
2015-07-30 02:16:37 +0700

    Add support for all possible ISO strings using regex
    Add corresponding testcases

--- spark/src/main/java/io/ddf/spark/etl/udf/DateParse.java
+++ spark/src/main/java/io/ddf/spark/etl/udf/DateParse.java
@@ -10,9 +10,13 @@ import org.joda.time.DateTimeZone;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * A SparkSQL UDF that convert a datetime string in a specific format into a datetime string
- * of format "yyyy-MM-dd HH:mm:ss" in UTC time zone
+ * of format "yyyy-MM-dd HH:mm:ss". In case of an ISO datetime string with timezone, the output will be a
+ * local datetime string in the corresponding timezone.
  * <p>
  * Examples:
  * <p>
@@ -36,7 +40,53 @@ public class DateParse {
       try {
         DateTime dt = null;
         if (format.equalsIgnoreCase("iso")) {
-          dt = new DateTime(string, DateTimeZone.UTC);
+          Pattern pattern = Pattern.compile(
+              "^(?<year>-?(?:[1-9][0-9]*)?[0-9]{4})(-(?<month>1[0-2]|0[1-9])(-"
+                  + "(?<day>3[01]|0[1-9]|[12][0-9])((?<sep>[T\\s])(?<hour>2[0-3]|[01][0-9])"
+                  + "(?<minute>:[0-5][0-9])((?<second>:[0-5][0-9])?(?<ms>\\.[0-9]+)?)?"
+                  + "(?<timezone>Z|\\s?[+-](?:2[0-3]|[01][0-9])(:?[0-5][0-9])?)?)?)?)?$"
+          );
+          Matcher matcher= pattern.matcher(string);
+          if (matcher.matches()) {
+            String year = matcher.group("year");
+            String month = matcher.group("month");
+            String day = matcher.group("day");
+            String sep = matcher.group("sep");
+            String hour = matcher.group("hour");
+            String minute = matcher.group("minute");
+            String second = matcher.group("second");
+            String ms = matcher.group("ms");
+            String timezone = matcher.group("timezone");
+
+            StringBuilder sb = new StringBuilder();
+            sb.append(year);
+            sb.append(month!=null?("-"+month):"");
+            sb.append(day!=null?("-"+day):"");
+            sb.append(sep!=null?sep:"");
+            sb.append(hour!=null?hour:"");
+            sb.append(minute!=null?(minute):"");
+            sb.append(second!=null?(second):"");
+            sb.append(ms!=null?(ms):"");
+            sb.append(timezone!=null?timezone:"");
+
+            StringBuilder sb2 = new StringBuilder();
+            sb2.append("yyyy");
+            sb2.append(month!=null?("-"+"MM"):"");
+            sb2.append(day!=null?("-"+"dd"):"");
+            sb2.append(sep!=null?sep.equalsIgnoreCase("T")?"'T'":sep:"");
+            sb2.append(hour!=null?"HH":"");
+            sb2.append(minute!=null?(":" + "mm"):"");
+            sb2.append(second!=null?(":" + "ss"):"");
+            sb2.append(ms!=null?("."+"SSS"):"");
+            sb2.append(timezone!=null?timezone.startsWith(" ")?" Z":"Z":"");
+
+            DateTimeFormatter formatter = DateTimeFormat.forPattern( sb2.toString());
+            dt = formatter.parseDateTime(sb.toString());
+
+          } else {
+            return null;
+          }
+
         } else {
           DateTimeFormatter formatter = DateTimeFormat.forPattern(format);
           dt = formatter.parseDateTime(string);
