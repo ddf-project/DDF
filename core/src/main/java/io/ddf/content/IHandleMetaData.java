5ca87798cddc3b97119b8279222405d24953406d
Dao Duc Huan <huan@adatao.com>
2015-09-15 16:45:35 +0700

    add additional methods to IHandleMetaData

--- core/src/main/java/io/ddf/content/IHandleMetaData.java
+++ core/src/main/java/io/ddf/content/IHandleMetaData.java
@@ -2,9 +2,11 @@ package io.ddf.content;
 
 
 import io.ddf.DDF;
+import io.ddf.datasource.DataSourceDescriptor;
 import io.ddf.exception.DDFException;
 import io.ddf.misc.IHandleDDFFunctionalGroup;
 
+import java.util.Date;
 import java.util.UUID;
 
 public interface IHandleMetaData extends IHandleDDFFunctionalGroup {
@@ -19,6 +21,33 @@ public interface IHandleMetaData extends IHandleDDFFunctionalGroup {
 
   public void copy(IHandleMetaData otherMetaData) throws DDFException;
 
+  public void setDataSourceDescriptor(DataSourceDescriptor dataSource);
+
+  public DataSourceDescriptor getDataSourceDescriptor();
+
+  public void setLastRefreshTime(Date time);
+
+  public Date getLastRefreshTime();
+
+  public void setLastRefreshUser(String user);
+
+  public String getLastRefreshUser();
+
+  public void setLastModifiedTime(Date time);
+
+  public Date getLastModifiedTime();
+
+  public void setLastModifiedUser(String user);
+
+  public String getLastModifiedUser();
+
+  public void setLastPersistedTime(Date time);
+
+  public Date getLastPersistedTime();
+
+  public void setSnapshotDescriptor(DataSourceDescriptor snapshotDescriptor);
+
+  public DataSourceDescriptor getSnapshotDescriptor();
   // return true if ddf is in use
   // false otherwise
   public boolean inUse();
