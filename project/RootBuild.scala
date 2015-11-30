122aac3f811dd3e0e27ef9484afcca0e9a2cc8d5
Binh Le <lebinh.it@gmail.com>
2015-11-30 14:28:54 +0700

    bump dev version to 1.4.7-SNAPSHOT

--- project/RootBuild.scala
+++ project/RootBuild.scala
@@ -30,7 +30,7 @@ object RootBuild extends Build {
   val rootOrganization = "io"
   val projectName = "ddf"
   val rootProjectName = projectName
-  val rootVersion = "1.4.6"
+  val rootVersion = "1.4.7-SNAPSHOT"
   //val rootVersion = if(YARN_ENABLED) {
   //  "1.2-adatao"
   //} else {
