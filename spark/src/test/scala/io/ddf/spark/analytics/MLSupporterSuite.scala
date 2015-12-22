6bcc9d88fdce733f9e6c3d29c123c5f3277c967f
bhan <bhan@adatao.com>
2015-12-22 15:42:23 -0800

    remove test

--- spark/src/test/scala/io/ddf/spark/analytics/MLSupporterSuite.scala
+++ spark/src/test/scala/io/ddf/spark/analytics/MLSupporterSuite.scala
@@ -4,7 +4,6 @@ import io.ddf.DDF
 import io.ddf.ml.IModel
 import io.ddf.spark.{ATestSuite, SparkDDF}
 import org.apache.spark.rdd._
-import org.apache.spark.mllib.feature.{Word2Vec, Word2VecModel}
 
 /**
   */
@@ -36,32 +35,5 @@ class MLSupporterSuite extends ATestSuite {
     manager.shutdown
   }
   */
-  test("test mllib word2vec") {
-    manager.sql("set shark.test.data.path=../resources", "SparkSQL")
-    manager.sql("drop table if exists text8small", "SparkSQL")
-    manager.sql("create table text8small (v1 string)" +
-      " row format delimited fields terminated by ','", "SparkSQL")
-    manager.sql("LOAD DATA LOCAL INPATH '${hiveconf:shark.test.data.path}/test/text8_small.txt' " +
-      "INTO TABLE text8small", "SparkSQL")
-  
-    val ddf: DDF = manager.sql2ddf("select * from text8small","SparkSQL")
-    val doc = ddf.asInstanceOf[SparkDDF].getRDD(classOf[Array[String]])
-    println(ddf.getNumRows)
-    println(ddf.getNumColumns)
-    println(doc.first)
-    val input = doc.map{line => line.toSeq}
-    val model = new Word2Vec().setMinCount(2).setVectorSize(10).setSeed(42L).fit(input)
-    
-    val kv = model.getVectors
-    println(kv.head)
-    println(kv.size)
-    println(kv.keys)
-    val syms = model.findSynonyms("feminism", 2) 
-    assert(syms.length == 2)
-    println(syms(0))
-    println(syms(1))
-    assert(syms(0)._2 > syms(1)._2)
-    manager.shutdown
-    
-  }
+
 }
