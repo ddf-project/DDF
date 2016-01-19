e54bd7912c6c305c0f7d71d7e23249c28d943038
jing <PangZhi@github.com>
2016-01-18 16:22:28 -0800

    move to httpclient

--- rest/src/main/java/io/ddf/rest/RestDDFManager.java
+++ rest/src/main/java/io/ddf/rest/RestDDFManager.java
@@ -8,19 +8,24 @@ import io.ddf.DDF;
 import io.ddf.DDFManager;
 import io.ddf.exception.DDFException;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Invocation;
-import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.UUID;
+
+
+import java.io.*;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 
 class RestDDFManager extends DDFManager {
 
     // Temp storage handler
     // Rest Client
-    Client client = ClientBuilder.newClient();
+    //Client client = ClientBuilder.newClient();
+    HttpClient httpClient = new DefaultHttpClient();
 
     public RestDDF newDDF(String restUri) throws DDFException {
         return new RestDDF(restUri);
@@ -33,6 +38,7 @@ class RestDDFManager extends DDFManager {
      * @throws DDFException
      */
     public String getData(RestDDF restDDF) throws DDFException {
+        /*
         WebTarget target = client.target(restDDF.getRestUri());
         Invocation.Builder request = target.request();
         request.accept(MediaType.APPLICATION_JSON_TYPE);
@@ -43,6 +49,39 @@ class RestDDFManager extends DDFManager {
             System.out.println(response.getEntity());
         }
         return response.getEntity().toString();
+        */
+        try {
+            HttpGet httpGetRequest = new HttpGet(restDDF.getRestUri());
+            HttpResponse httpResponse = httpClient.execute(httpGetRequest);
+
+            System.out.println("----------------------------------------");
+            System.out.println(httpResponse.getStatusLine());
+            System.out.println("----------------------------------------");
+
+            HttpEntity entity = httpResponse.getEntity();
+
+            byte[] buffer = new byte[1024];
+            if (entity != null) {
+                InputStream inputStream = entity.getContent();
+                try {
+                    int bytesRead = 0;
+                    BufferedInputStream bis = new BufferedInputStream(inputStream);
+                    while ((bytesRead = bis.read(buffer)) != -1) {
+                        String chunk = new String(buffer, 0, bytesRead);
+                        System.out.println(chunk);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    try { inputStream.close(); } catch (Exception ignore) {}
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            httpClient.getConnectionManager().shutdown();
+        }
+        return null;
     }
 
 
--- rest/src/test/java/io/ddf/rest/RestDDFManagerTests.java
+++ rest/src/test/java/io/ddf/rest/RestDDFManagerTests.java
@@ -1,6 +1,5 @@
 package io.ddf.rest;
 
-import com.sun.deploy.net.HttpResponse;
 import io.ddf.DDF;
 import io.ddf.exception.DDFException;
 import junit.framework.Assert;
@@ -8,6 +7,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 
 /**
  * Created by jing on 1/18/16.
@@ -35,8 +35,8 @@ public class RestDDFManagerTests {
 
     @Test
     public void testGetData() throws DDFException {
-        RestDDF ddf = restDDFManager.newDDF("api.openweathermap.org/data/2.5/weather?lat=35&lon=139");
-
+        RestDDF ddf = restDDFManager.newDDF("http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=2de143494c0b295cca9337e1e96b00e0");
+        restDDFManager.getData(ddf);
     }
 
 }
