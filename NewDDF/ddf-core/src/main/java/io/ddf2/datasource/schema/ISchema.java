cdfa1ab8712e6a6091ceaa2ccb290a14121b96ba
jing <PangZhi@github.com>
2016-01-25 13:58:14 -0800

    fill content in viewhandler, many parts are not finished because of basic functions missing

--- NewDDF/ddf-core/src/main/java/io/ddf2/datasource/schema/ISchema.java
+++ NewDDF/ddf-core/src/main/java/io/ddf2/datasource/schema/ISchema.java
@@ -1,12 +1,23 @@
 package io.ddf2.datasource.schema;
 
+import io.ddf2.DDFException;
+
 import java.util.List;
+import java.util.Set;
 
 public interface ISchema {
 
 	public int getNumColumn() ;
 	 
 	public List<IColumn> getColumns();
+
+	public IColumn getColumn(String columnName) throws DDFException;
+
+	public List<String> getColumnNames();
+
+	public String getColumnName(int index) throws DDFException;
+
+	public int getColumnIndex(String columnName) throws DDFException;
 	 
 }
  
