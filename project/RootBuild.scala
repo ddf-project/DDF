255a2292e5ac7a7ff8cf4ba4f145ed3f880637dd
SangDang <zkidkid@gmail.com>
2016-04-05 22:21:50 -0700

    udpate RootBuild

--- project/RootBuild.scala
+++ project/RootBuild.scala
@@ -15,7 +15,7 @@ object RootBuild extends Build {
   val OBSELETE_HADOOP_VERSION = "1.0.4"
   val DEFAULT_HADOOP_VERSION = "2.2.0"
 
-  val SPARK_VERSION = "1.6.0-adatao-hd2.7.2"
+  val SPARK_VERSION = "1.6.0-adatao-1.7.0"
 
   val YARN_ENABLED = env("SPARK_YARN").getOrElse("true").toBoolean
 
@@ -128,7 +128,7 @@ object RootBuild extends Build {
     //"org.apache.derby" % "derby" % "10.4.2.0",
    // "org.apache.spark" % "spark-streaming_2.10" % SPARK_VERSION excludeAll(excludeSpark),
     "org.apache.spark" % "spark-core_2.10" % SPARK_VERSION  exclude("net.java.dev.jets3t", "jets3t") exclude("com.google.protobuf", "protobuf-java") exclude ("com.google.code.findbugs", "jsr305")
-      exclude("io.netty", "netty-all") exclude("org.mortbay.jetty", "jetty"),
+      exclude("org.jboss.netty", "netty") exclude("org.mortbay.jetty", "jetty"),
     //"org.apache.spark" % "spark-repl_2.10" % SPARK_VERSION excludeAll(excludeSpark) exclude("com.google.protobuf", "protobuf-java") exclude("io.netty", "netty-all") exclude("org.jboss.netty", "netty"),
     "org.apache.spark" % "spark-mllib_2.10" % SPARK_VERSION excludeAll(excludeSpark) exclude("io.netty", "netty-all"),
     "org.apache.spark" % "spark-sql_2.10" % SPARK_VERSION exclude("io.netty", "netty-all")
@@ -144,7 +144,7 @@ object RootBuild extends Build {
   )
 
   val hdfs_dependencies = Seq(
-    "org.apache.hadoop" % "hadoop-hdfs" % "2.7.2"
+    "org.apache.hadoop" % "hadoop-hdfs" % "2.2.0"
   )
 
   val test_dependencies = Seq(
@@ -271,7 +271,6 @@ object RootBuild extends Build {
     dependencyOverrides += "io.dropwizard.metrics" % "metrics-json" % "3.1.2",
     dependencyOverrides += "io.dropwizard.metrics" % "metrics-jvm" % "3.1.2",
     dependencyOverrides += "org.apache.commons" % "commons-lang3" % "3.1",
-    dependencyOverrides += "org.apache.curator" % "curator-recipes" % "2.7.1",
       pomExtra := (
       <!--
       **************************************************************************************************
@@ -467,8 +466,8 @@ object RootBuild extends Build {
     // Add post-compile activities: touch the maven timestamp files so mvn doesn't have to compile again
     compile in Compile <<= compile in Compile andFinally { List("sh", "-c", "touch core/" + targetDir + "/*timestamp") },
     libraryDependencies += "org.xerial" % "sqlite-jdbc" % "3.7.2",
-    libraryDependencies += "com.google.code.findbugs" % "jsr305" % "3.0.0",
-    libraryDependencies += "org.apache.hadoop" % "hadoop-common" % "2.7.2" exclude("org.mortbay.jetty", "servlet-api")
+    libraryDependencies += "com.google.code.findbugs" % "jsr305" % "2.0.1",
+    libraryDependencies += "org.apache.hadoop" % "hadoop-common" % "2.2.0" exclude("org.mortbay.jetty", "servlet-api")
       exclude("javax.servlet", "servlet-api"),
     libraryDependencies += "org.jgrapht" % "jgrapht-core" % "0.9.0",
     libraryDependencies ++= scalaDependencies,
