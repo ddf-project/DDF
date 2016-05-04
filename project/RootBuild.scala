e684bc9a3b8bc5cef24e8813080c028e8dc75f21
SangDang <zkidkid@gmail.com>
2016-05-04 10:49:43 +0700

    update csv-version

--- project/RootBuild.scala
+++ project/RootBuild.scala
@@ -24,7 +24,7 @@ object RootBuild extends Build {
   val OBSELETE_HADOOP_VERSION = "1.0.4"
   val DEFAULT_HADOOP_VERSION = "2.2.0"
   val SPARK_VERSION = "1.6.0-adatao-hd2.7.2"
-  val SPARK_CSV_VERSION = "arimo-1.4.0.4"
+  val SPARK_CSV_VERSION = "arimo-1.4.0.5"
   val YARN_ENABLED = env("SPARK_YARN").getOrElse("true").toBoolean
   //val rootVersion = if(YARN_ENABLED) {
   //  "1.2-adatao"
@@ -108,7 +108,7 @@ object RootBuild extends Build {
 
   val spark_dependencies = Seq(
     "io.ddf" % "ddf_hdfs_2.10" % rootVersion exclude("javax.servlet", "servlet-api"),
-    "com.databricks" % "spark-csv_2.11" % SPARK_CSV_VERSION excludeAll(excludeSpark),
+    "com.databricks" % "spark-csv_2.11" % SPARK_CSV_VERSION excludeAll(excludeSpark) changing() ,
     "com.databricks" % "spark-avro_2.10" % "2.0.1",
     "commons-configuration" % "commons-configuration" % "1.6",
     "com.google.code.gson"% "gson" % "2.2.2",
