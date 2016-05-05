ec2b567e79f14da037909ac9d83a27977fae69e7
SangDang <zkidkid@gmail.com>
2016-05-05 14:09:35 +0700

    update applySchema test

--- spark/src/test/java/io/ddf/spark/content/SchemaHandlerTest.java
+++ spark/src/test/java/io/ddf/spark/content/SchemaHandlerTest.java
@@ -24,7 +24,6 @@ public class SchemaHandlerTest extends BaseTest {
 
   /**
    * Create table with some type for dataingestion
-   * @throws DDFException
    */
   @BeforeClass
   public static void beforeClass() throws DDFException {
@@ -33,8 +32,8 @@ public class SchemaHandlerTest extends BaseTest {
     manager.sql("drop table if exists DataIngestion", "SparkSQL");
 
     manager.sql("create table DataIngestion (type_string string,type_boolean string,type_short string, type_long string,"
-                                      + "type_double string,type_decimal string,type_date string,type_timestamp string) "
-                                      + "ROW FORMAT DELIMITED FIELDS TERMINATED BY ','", "SparkSQL");
+        + "type_double string,type_decimal string,type_date string,type_timestamp string) "
+        + "ROW FORMAT DELIMITED FIELDS TERMINATED BY ','", "SparkSQL");
 
     manager.sql("load data local inpath 'resources/test/data4ingestion.csv' into table DataIngestion", "SparkSQL");
   }
@@ -57,9 +56,10 @@ public class SchemaHandlerTest extends BaseTest {
 
     Schema applySchema = new Schema(applyColumns);
 
-    testApplyWithDrop(ddf,applySchema);
-      testApplyFailFast(ddf, applySchema);
-    testApplyWithoutDrop(ddf,applySchema);
+    testApplyWithDrop(ddf, applySchema);
+    testApplyFailFast(ddf, applySchema);
+    testApplyWithoutDrop(ddf, applySchema);
+    testApplyWithoutDropWithSample(ddf,applySchema);
     System.out.println("End Test ApplySchemaStatistic");
 
 
@@ -67,57 +67,94 @@ public class SchemaHandlerTest extends BaseTest {
 
   private void testApplyWithoutDrop(SparkDDF ddf, Schema schema) throws DDFException {
     SchemaHandler schemaHandler = (SchemaHandler) ddf.getSchemaHandler();
-    Tuple2<SparkDDF, SchemaHandler.ApplySchemaStatistic> applySchema = schemaHandler.applySchema(schema,"SET_NONE");
+    Tuple2<SparkDDF, SchemaHandler.ApplySchemaStatistic> applySchema = schemaHandler.applySchema(schema, "SETNONE");
     SparkDDF sparkDDF = applySchema._1();
     SchemaHandler.ApplySchemaStatistic statistic = applySchema._2();
-    statistic.getMapColumnStatistic().forEach( (colName,colStats) -> {
+    statistic.getMapColumnStatistic().forEach((colName, colStats) -> {
       System.out.println("column: " + colName);
       System.out.println("numFailed: " + colStats.getNumConvertedFailed());
       System.out.println("num sample" + colStats.getNumSampleData());
       System.out.println("sample" + Arrays.toString(colStats.getSampleData().toArray()));
     });
-    assertEquals((long)statistic.getTotalLineProcessed(),2L);
-    assertEquals((long)statistic.getTotalLineSuccessed(),2L);
+    assertEquals((long) statistic.getTotalLineProcessed(), 16L);
+    assertEquals((long) statistic.getTotalLineSuccessed(), 16L);
     List<Schema.Column> newColumn = sparkDDF.getSchema().getColumns();
-    assertEqualColumn(schema.getColumns(),newColumn);
+    assertEqualColumn(schema.getColumns(), newColumn);
+
+  }
+  private void testApplyWithoutDropWithSample(SparkDDF ddf, Schema schema) throws DDFException {
+    SchemaHandler schemaHandler = (SchemaHandler) ddf.getSchemaHandler();
+    int sample = 10;
+    Tuple2<SparkDDF, SchemaHandler.ApplySchemaStatistic> applySchema = schemaHandler.applySchema(schema, "SETNONE",sample);
+    SparkDDF sparkDDF = applySchema._1();
+    SchemaHandler.ApplySchemaStatistic statistic = applySchema._2();
+    statistic.getMapColumnStatistic().forEach((colName, colStats) -> {
+      System.out.println("column: " + colName);
+      System.out.println("numFailed: " + colStats.getNumConvertedFailed());
+      System.out.println("num sample" + colStats.getNumSampleData());
+      System.out.println("sample" + Arrays.toString(colStats.getSampleData().toArray()));
+    });
+    assertEquals((long) statistic.getTotalLineProcessed(), sample);
+    assertEquals((long) statistic.getTotalLineSuccessed(), sample);
+    List<Schema.Column> newColumn = sparkDDF.getSchema().getColumns();
+    assertEqualColumn(schema.getColumns(), newColumn);
 
   }
 
-  private void testApplyWithDrop(SparkDDF ddf,Schema schema) throws DDFException {
+  private void testApplyWithDrop(SparkDDF ddf, Schema schema) throws DDFException {
 
     SchemaHandler schemaHandler = (SchemaHandler) ddf.getSchemaHandler();
-    Tuple2<SparkDDF, SchemaHandler.ApplySchemaStatistic> applySchema = schemaHandler.applySchema(schema,"DROP_ROW");
+    Tuple2<SparkDDF, SchemaHandler.ApplySchemaStatistic> applySchema = schemaHandler.applySchema(schema, "DROPMALFORMED");
     SparkDDF sparkDDF = applySchema._1();
     SchemaHandler.ApplySchemaStatistic statistic = applySchema._2();
-    statistic.getMapColumnStatistic().forEach( (colName,colStats) -> {
+    statistic.getMapColumnStatistic().forEach((colName, colStats) -> {
       System.out.println("column: " + colName);
       System.out.println("numFailed: " + colStats.getNumConvertedFailed());
       System.out.println("num sample" + colStats.getNumSampleData());
       System.out.println("sample" + Arrays.toString(colStats.getSampleData().toArray()));
     });
-    assertEquals((long)statistic.getTotalLineProcessed(),2L);
-    assertEquals((long)statistic.getTotalLineSuccessed(),1L);
+    assertEquals((long) statistic.getTotalLineProcessed(), 16L);
+    assertEquals((long) statistic.getTotalLineSuccessed(), 8L);
     List<Schema.Column> newColumn = sparkDDF.getSchema().getColumns();
-    assertEqualColumn(schema.getColumns(),newColumn);
+    assertEqualColumn(schema.getColumns(), newColumn);
   }
-  private void testApplyFailFast(SparkDDF ddf,Schema schema) throws DDFException {
+  private void testApplyWithDropWithSample(SparkDDF ddf, Schema schema) throws DDFException {
+    int sample =10;
+    SchemaHandler schemaHandler = (SchemaHandler) ddf.getSchemaHandler();
+    Tuple2<SparkDDF, SchemaHandler.ApplySchemaStatistic> applySchema = schemaHandler.applySchema(schema, "DROPMALFORMED",sample);
+    SparkDDF sparkDDF = applySchema._1();
+    SchemaHandler.ApplySchemaStatistic statistic = applySchema._2();
+    statistic.getMapColumnStatistic().forEach((colName, colStats) -> {
+      System.out.println("column: " + colName);
+      System.out.println("numFailed: " + colStats.getNumConvertedFailed());
+      System.out.println("num sample" + colStats.getNumSampleData());
+      System.out.println("sample" + Arrays.toString(colStats.getSampleData().toArray()));
+    });
+    assertEquals((long) statistic.getTotalLineProcessed(), sample);
+    assertEquals((long) statistic.getTotalLineSuccessed(), sample/2);
+    List<Schema.Column> newColumn = sparkDDF.getSchema().getColumns();
+    assertEqualColumn(schema.getColumns(), newColumn);
+  }
+
+  private void testApplyFailFast(SparkDDF ddf, Schema schema) throws DDFException {
 
     SchemaHandler schemaHandler = (SchemaHandler) ddf.getSchemaHandler();
     try {
-      Tuple2<SparkDDF, SchemaHandler.ApplySchemaStatistic> applySchema = schemaHandler.applySchema(schema, "FAIL_FAST");
-      assertTrue("Not Reach Here",false);
-    }catch(DDFException ex){
-      assertTrue("Fail exception",ex.getMessage().contains("FailFast"));
+      Tuple2<SparkDDF, SchemaHandler.ApplySchemaStatistic> applySchema = schemaHandler.applySchema(schema, "FAILFAST");
+      assertTrue("Not Reach Here", false);
+    } catch (DDFException ex) {
+      assertTrue("Fail exception", ex.getMessage().contains("FailFast"));
     }
 
 
   }
-  private void assertEqualColumn(List<Schema.Column> expectedColumn, List<Schema.Column> actualColumns){
+
+  private void assertEqualColumn(List<Schema.Column> expectedColumn, List<Schema.Column> actualColumns) {
     assertEquals(actualColumns.size(), expectedColumn.size());
-    for(int i=0; i < actualColumns.size();++i){
-      assertEquals(expectedColumn.get(i).getName(),actualColumns.get(i).getName());
-      assertEquals(expectedColumn.get(i).getType(),actualColumns.get(i).getType());
-      System.out.println(actualColumns.get(i).getName()+ " : " +actualColumns.get(i).getType());
+    for (int i = 0; i < actualColumns.size(); ++i) {
+      assertEquals(expectedColumn.get(i).getName(), actualColumns.get(i).getName());
+      assertEquals(expectedColumn.get(i).getType(), actualColumns.get(i).getType());
+      System.out.println(actualColumns.get(i).getName() + " : " + actualColumns.get(i).getType());
     }
   }
 
