98fb18334f2bed9f336ffd97e37e38e3500dfc73
Dao Duc Huan <huan@adatao.com>
2016-05-19 10:57:09 -0700

    add default implementation for IGloballyAddressable

---
+++ core/src/test/java/io/ddf/types/GloballyAddressableTest.java
@@ -0,0 +1,29 @@
+package io.ddf.types;
+
+import io.ddf.exception.DDFException;
+import org.junit.Test;
+import junit.framework.Assert;
+import io.ddf.DDFManager;
+/**
+ */
+public class GloballyAddressableTest {
+
+  public class MyClass extends GloballyAddressable {
+
+  }
+
+  @Test
+  public void testGloballyAddressable() throws DDFException {
+    GloballyAddressable instance = new MyClass();
+    instance.setName("myName");
+    Assert.assertEquals(instance.getNamespace(), "adatao");
+    Assert.assertEquals(instance.getName(), "myName");
+    Assert.assertEquals(instance.getUri(), "MyClass://adatao/myName");
+    Assert.assertTrue(instance.getUUID() != null);
+    DDFManager manager = DDFManager.get(DDFManager.EngineType.BASIC);
+    manager.REGISTRY.register(instance);
+
+    IGloballyAddressable object = manager.REGISTRY.retrieve("adatao", "myName");
+    Assert.assertTrue(object instanceof MyClass);
+  }
+}
