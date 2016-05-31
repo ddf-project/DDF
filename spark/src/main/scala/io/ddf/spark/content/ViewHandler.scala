489b3e05cda7b2be4931fe9bc1c21f7d523f22f2
Nhan Vu <nhanitvn@users.noreply.github.com>
2016-05-31 11:34:36 +0700

    Better check for integer number of samples

--- spark/src/main/scala/io/ddf/spark/content/ViewHandler.scala
+++ spark/src/main/scala/io/ddf/spark/content/ViewHandler.scala
@@ -76,8 +76,7 @@ class ViewHandler(mDDF: DDF) extends io.ddf.content.ViewHandler(mDDF) with IHand
     val schema: Schema = new Schema(null,
       JavaConverters.asScalaBufferConverter(mDDF.getSchema.getColumns).asScala.toArray)
     // Optimized running time for integer values by using Spark DataFrame sample and limit APIs
-    val numSamplesInt = numSamples.toInt
-    val sampleDDF = if (numSamplesInt == numSamples) {
+    val sampleDDF = if (numSamples <= Int.MaxValue.toLong) {
       val sparkDF = mDDF.getRepresentationHandler.get(classOf[DataFrame]).asInstanceOf[DataFrame]
       val sampleDF = sparkDF.sample(withReplacement, fraction, seed).limit(numSamples.toInt)
 
