ce0fa917bde1fec5f65c770abf5e5ab01aaa405c
Dao Duc Huan <huan@adatao.com>
2016-05-31 22:50:13 -0700

    store DDF with name on a separate cache

--- core/src/main/java/io/ddf/DDFCache.java
+++ core/src/main/java/io/ddf/DDFCache.java
@@ -3,7 +3,11 @@ package io.ddf;
 
 import com.google.common.base.Strings;
 import com.google.common.cache.*;
+import com.google.common.util.concurrent.Striped;
 import io.ddf.exception.DDFException;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
@@ -13,21 +17,40 @@ import io.ddf.misc.Config;
 import org.apache.commons.lang.exception.ExceptionUtils;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
 
 /**
  */
 public class DDFCache extends ALoggable {
 
-  private LoadingCache<UUID, DDF> mDDFCache;
+  Striped<Lock> uuidLocks = Striped.lock(Runtime.getRuntime().availableProcessors() * 4);
+
+  /**
+   * Keeping DDF without name
+   */
+  private Cache<UUID, DDF> mDDFCache;
+
+  /**
+   * Keeping DDF that has name in a separate cache, so eviction on DDF without name
+   * does not affect DDF with name
+   */
+  private Cache<UUID, DDF> mDDFwithNameCache;
 
   private DDFManager mDDFManager;
 
   public DDFCache(DDFManager manager) {
+
     mDDFManager = manager;
+
     /**
-     * Maximum number of DDFs allowed in cache, DDFs will be evicted by an approximate LRU algorithm
+     * Maximum number of DDFs without name allowed in cache, DDFs will be evicted by an approximate LRU algorithm
      */
     Long maxNumberOfDDFs = Long.valueOf(Config.getGlobalValue(Config.ConfigConstant.MAX_NUMBER_OF_DDFS_IN_CACHE));
+
+    /**
+     * Maximum number of DDFs with name allowed in cache, DDFs with name will be evicted by an approximate LRU algorithm
+     */
+    Long maxNumberOfDDFsWName = Long.valueOf(Config.getGlobalValue(Config.ConfigConstant.MAX_NUMBER_OF_DDFS_WITH_NAME_IN_CACHE));
 
     /**
      * The number of seconds that the DDF will live in cache since it last used(meaning inserted or accessed)
@@ -36,24 +59,20 @@ public class DDFCache extends ALoggable {
 
     mLog.info(String.format("Maximum number of ddfs in cache %s", maxNumberOfDDFs));
     mLog.info(String.format("DDF's time to idle = %s seconds", timeToIdle));
+
     mDDFCache = CacheBuilder.newBuilder().
         maximumSize(maxNumberOfDDFs).expireAfterAccess(timeToIdle, TimeUnit.SECONDS).
         recordStats().removalListener(new DDFRemovalListener())
-        .build(new CacheLoader<UUID, DDF>() {
-      @Override public DDF load(UUID uuid) throws Exception {
-        try {
-          mLog.info(String.format("restoring ddf %s", uuid));
-          return mDDFManager.restoreDDF(uuid);
-        } catch (Exception e) {
-          mLog.error(String.format("Error restoring DDF %s, error = %s", uuid, ExceptionUtils.getStackTrace(e)));
-          throw new DDFException(String.format("DDF with uuid %s does not exist", uuid));
-        }
-      }
-    });
+        .build();
+
+    // Don't do time based eviction for DDF with name
+    mDDFwithNameCache = CacheBuilder.newBuilder().
+        maximumSize(maxNumberOfDDFsWName).recordStats().removalListener(new DDFRemovalListener())
+        .build();
   }
 
   private boolean containsDDF(UUID uuid) {
-    return mDDFCache.asMap().containsKey(uuid);
+    return (mDDFCache.asMap().containsKey(uuid) || mDDFwithNameCache.asMap().containsKey(uuid));
   }
 
   private Map<String, UUID> mUris = new ConcurrentHashMap<String, UUID>();
@@ -62,26 +81,83 @@ public class DDFCache extends ALoggable {
     if(ddf.getUUID() == null) {
       throw new DDFException("uuid is null");
     }
-    mDDFCache.put(ddf.getUUID(), ddf);
+    Lock lock = uuidLocks.get(ddf.getUUID());
+    try {
+      if(lock.tryLock(120, TimeUnit.SECONDS)) {
+        try {
+          if (ddf.getName() == null) {
+            mDDFCache.put(ddf.getUUID(), ddf);
+          } else {
+            mDDFwithNameCache.put(ddf.getUUID(), ddf);
+          }
+        } finally {
+          lock.unlock();
+        }
+      } else {
+        throw new DDFException("Timeout getting DDF");
+      }
+    } catch (InterruptedException e) {
+
+    }
   }
 
   public CacheStats getCacheStats() {
-    return mDDFCache.stats();
+    return mDDFCache.stats().plus(mDDFwithNameCache.stats());
   }
 
-  public void removeDDF(DDF ddf) throws DDFException {
+  public synchronized void removeDDF(DDF ddf) throws DDFException {
     mDDFCache.invalidate(ddf.getUUID());
+    mDDFwithNameCache.invalidate(ddf.getUUID());
     if(ddf.getUri() != null) {
       mUris.remove(ddf.getUri());
     }
   }
 
   public DDF[] listDDFs() {
-    return mDDFCache.asMap().values().toArray(new DDF[]{});
+    List<DDF> ddfs = new ArrayList<DDF>();
+    ddfs.addAll(mDDFCache.asMap().values());
+    ddfs.addAll(mDDFwithNameCache.asMap().values());
+    return ddfs.toArray(new DDF[]{});
   }
 
-  public DDF getDDF(UUID uuid) {
-    return mDDFCache.getUnchecked(uuid);
+  public long size() {
+    return mDDFCache.size() + mDDFwithNameCache.size();
+  }
+
+  /**
+   *  Try to get DDF from both mDDFCache and mDDFWithNameCache
+   *  if DDF is not present in both, do a restore and put it into the respective cache
+   * @param uuid
+   * @return
+   * @throws DDFException
+   */
+  public DDF getDDF(UUID uuid) throws DDFException {
+    Lock lock = uuidLocks.get(uuid);
+    try {
+      if (lock.tryLock(120, TimeUnit.SECONDS)) {
+        try {
+          DDF ddf = mDDFCache.getIfPresent(uuid);
+          if (ddf == null) {
+            ddf = mDDFwithNameCache.getIfPresent(uuid);
+            if (ddf == null) {
+              ddf = mDDFManager.restoreDDF(uuid);
+            }
+            if (ddf.getName() != null) {
+              mDDFwithNameCache.put(uuid, ddf);
+            } else {
+              mDDFCache.put(uuid, ddf);
+            }
+          }
+          return ddf;
+        } finally {
+          lock.unlock();
+        }
+      } else {
+        throw new DDFException("Timeout getting DDF");
+      }
+    } catch (InterruptedException e) {
+      throw new DDFException("Error getting DDF ", e);
+    }
   }
 
   public DDF getDDFByName(String name) throws DDFException {
@@ -94,14 +170,19 @@ public class DDFCache extends ALoggable {
   }
 
   public synchronized void setDDFName(DDF ddf, String name) throws DDFException {
-    if(!Strings.isNullOrEmpty(name)) {
-      if(!Strings.isNullOrEmpty(ddf.getName())) {
-        this.mUris.remove(ddf.getUri());
+    synchronized (ddf) {
+      if (!Strings.isNullOrEmpty(name)) {
+        if (!Strings.isNullOrEmpty(ddf.getName())) {
+          this.mUris.remove(ddf.getUri());
+        }
+        ddf.setName(name);
+
+        mDDFCache.invalidate(ddf.getUUID());
+        mDDFwithNameCache.put(ddf.getUUID(), ddf);
+        this.mUris.put(ddf.getUri(), ddf.getUUID());
+      } else {
+        throw new DDFException(String.format("DDF's name cannot be null or empty"));
       }
-      ddf.setName(name);
-      this.mUris.put(ddf.getUri(), ddf.getUUID());
-    } else {
-      throw new DDFException(String.format("DDF's name cannot be null or empty"));
     }
   }
 
@@ -112,10 +193,14 @@ public class DDFCache extends ALoggable {
       UUID prevUUID = ddf.getUUID();
       ddf.setUUID(uuid);
       if(prevUUID != null) {
-        mDDFCache.invalidate(prevUUID);
-        mDDFCache.put(uuid, ddf);
+        if(ddf.getName() == null) {
+          mDDFCache.invalidate(prevUUID);
+          mDDFCache.put(uuid, ddf);
+        } else {
+          mDDFwithNameCache.invalidate(prevUUID);
+          mDDFCache.put(uuid, ddf);
+        }
         if(ddf.getUri() != null) {
-          mUris.remove(ddf.getUri());
           mUris.put(ddf.getUri(), ddf.getUUID());
         }
       }
@@ -139,10 +224,28 @@ public class DDFCache extends ALoggable {
         mLog.info(String.format("CacheStats = %s", getCacheStats().toString()));
         DDF ddf = notification.getValue();
         if (ddf != null) {
-          mLog.info(String.format("Removing DDF %s", ddf.getUUID()));
+          if(ddf.getName() != null) {
+            mLog.info(String.format("Removing DDF %s, name = %s", ddf.getUUID(), ddf.getName()));
+          } else {
+            mLog.info(String.format("Removing DDF %s", ddf.getUUID()));
+          }
           ddf.cleanup();
         }
       }
     }
   }
+//
+//  private class DDFLoader extends CacheLoader<UUID, DDF> {
+//
+//    @Override
+//    public DDF load(UUID uuid) throws Exception {
+//      try {
+//        mLog.info(String.format("restoring ddf %s", uuid));
+//        return mDDFManager.restoreDDF(uuid);
+//      } catch (Exception e) {
+//        mLog.error(String.format("Error restoring DDF %s, error = %s", uuid, ExceptionUtils.getStackTrace(e)));
+//        throw new DDFException(String.format("DDF with uuid %s does not exist", uuid));
+//      }
+//    }
+//  }
 }
--- core/src/test/java/io/basic/ddf/content/DDFCacheTests.java
+++ core/src/test/java/io/basic/ddf/content/DDFCacheTests.java
@@ -34,6 +34,12 @@ public class DDFCacheTests {
     return ddf;
   }
 
+  private DDF getTestDDF(DDFManager manager, String name) throws DDFException {
+    DDF ddf = this.getTestDDF(manager);
+    manager.setDDFName(ddf, name);
+    return ddf;
+  }
+
   /**
    * Trigger a GC and make sure it run
    */
@@ -94,6 +100,80 @@ public class DDFCacheTests {
         removed += 1;
       }
     }
+    Assert.assertTrue(removed > 0);
+    Assert.assertTrue(manager.getDDFcache().getCacheStats().evictionCount() > 0);
+  }
+
+  @Test
+  public void keepingDDFwithName() throws Exception {
+    ArrayList<WeakReference<DDF>> listDDFs = new ArrayList<WeakReference<DDF>>();
+    Config.set(ConfigConstant.SECTION_GLOBAL.toString(), ConfigConstant.MAX_NUMBER_OF_DDFS_IN_CACHE.toString(), "4000");
+    Config.set(ConfigConstant.SECTION_GLOBAL.toString(), ConfigConstant.DDF_TIME_TO_IDLE_SECONDS.toString(), "1");
+    DDFManager manager = getDDFManager();
+
+    listDDFs.add(new WeakReference<DDF>(getTestDDF(manager, "myname")));
+    listDDFs.add(new WeakReference<DDF>(getTestDDF(manager, "myname1")));
+
+    int i = 0;
+    while(i < 10) {
+      listDDFs.add(new WeakReference<DDF>(getTestDDF(manager)));
+      i += 1;
+    }
+
+    Thread.sleep(2100);
+    // eviction is triggered here
+    getTestDDF(manager);
+    getTestDDF(manager);
+    getTestDDF(manager);
+    getTestDDF(manager);
+    runGC();
+    // count number of ddfs that were gced
+    int removed = 0;
+    for(WeakReference<DDF> wDDF: listDDFs) {
+      if(wDDF.get() == null) {
+        removed += 1;
+      }
+    }
+    DDF ddf0 = listDDFs.get(0).get();
+    DDF ddf1 = listDDFs.get(1).get();
+    Assert.assertTrue(ddf0 != null);
+    Assert.assertTrue(ddf1 != null);
+    Assert.assertTrue(ddf0.getName() == "myname");
+    Assert.assertTrue(ddf1.getName() == "myname1");
+    Assert.assertTrue(removed > 0);
+    Assert.assertTrue(manager.getDDFcache().getCacheStats().evictionCount() > 0);
+  }
+
+  @Test
+  public void removeDDFWithName() throws Exception {
+    ArrayList<WeakReference<DDF>> listDDFs = new ArrayList<WeakReference<DDF>>();
+    Config.set(ConfigConstant.SECTION_GLOBAL.toString(), ConfigConstant.MAX_NUMBER_OF_DDFS_WITH_NAME_IN_CACHE.toString(), "10");
+    Config.set(ConfigConstant.SECTION_GLOBAL.toString(), ConfigConstant.MAX_NUMBER_OF_DDFS_IN_CACHE.toString(), "4000");
+
+    DDFManager manager = getDDFManager();
+
+    int i = 0;
+    while(i < 20) {
+      String name = String.format("myname%s", i);
+      listDDFs.add(new WeakReference<DDF>(getTestDDF(manager, name)));
+      i += 1;
+    }
+
+    int j = 0;
+    while(j < 20) {
+      listDDFs.add(new WeakReference<DDF>(getTestDDF(manager)));
+      j += 1;
+    }
+
+    runGC();
+    // count number of ddfs that were gced
+    int removed = 0;
+    for(WeakReference<DDF> wDDF: listDDFs) {
+      if(wDDF.get() == null) {
+        removed += 1;
+      }
+    }
+
     Assert.assertTrue(removed == 10);
     Assert.assertTrue(manager.getDDFcache().getCacheStats().evictionCount() == 10);
   }
